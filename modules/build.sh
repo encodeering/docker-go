9a9513928aad3ec56d6a25e9440d5d9f15561281
Michael Clausen <encodeering@gmail.com>
2018-01-03 13:03:11 +0100

    adds a build definition for go
    * strech should be used, but is not available yet, so we use jessie instead as we do not expect missing dependencies here

--- modules/build.sh
+++ modules/build.sh
@@ -4,3 +4,14 @@
 #   env.PROJECT
 #   env.VERSION
 #   env.REPOSITORY
+
+set -e
+
+import com.encodeering.docker.config
+import com.encodeering.docker.docker
+
+docker-pull "$REPOSITORY/buildpack-$ARCH:jessie-scm" "buildpack-deps:stretch-scm"
+
+docker build -t "$DOCKER_IMAGE" "$PROJECT/$VERSION/stretch"
+
+docker run --rm "$DOCKER_IMAGE" go version
