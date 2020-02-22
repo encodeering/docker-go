f9e8073d74d9f2d40a8adff305ac96611df96f9d
Michael Clausen <encodeering@gmail.com>
2020-02-22 16:11:19 +0100

    updating scm repository to latest revision
    * ffe8dcb44eb16cabd71b8b74cbf0d92f244afd53
    
    NOTE
    
    * go 1.11 is not available anymore, update to proper base images for go 1.13

--- modules/build-debian.sh
+++ modules/build-debian.sh
@@ -5,6 +5,6 @@ set -e
 import com.encodeering.ci.config
 import com.encodeering.ci.docker
 
-docker-pull "$REPOSITORY/buildpack-$ARCH:stretch-scm" "buildpack-deps:stretch-scm"
+docker-pull "$REPOSITORY/buildpack-$ARCH:buster-scm" "buildpack-deps:buster-scm"
 
-docker-build "$PROJECT/$VERSION/stretch"
+docker-build "$PROJECT/$VERSION/buster"
